264400b1e06a6c00665e44e516648ba53b8213fa
Leslie <leshermira@gmail.com>
2020-05-18 21:45:19 -0400

    version2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let pronoun = ['El','Nuestro', ];
+let pronoun = ['El','Nuestro', 'Un', 'Este'];
 let adj = ['gran', 'super', 'genial', 'colosal' ];
 let noun = ['castor','animal','perro', 'arbol'];
 
 function domainGenerator (){
-    for(let i=0; i<3; i++){
+    for(let i=0; i<4; i++){
         alert('www.' + pronoun[i] + adj[i] + noun[i] + '.com');
         alert('www.' + adj[i] + pronoun[i] + noun[i] + '.com');
         alert('www.' + pronoun[i] + noun[i] + adj[i] + '.com'); //aca tiene un problema ya que me aparece "Undefined"
